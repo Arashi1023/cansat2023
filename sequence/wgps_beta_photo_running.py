b4038e0f5d22c4a2055443eb076a6140b1254191
hayaocci <7319079@ed.tus.ac.jp>
2023-08-28 14:19:45 +0900

    a

--- sequence/wgps_beta_photo_running.py
+++ sequence/wgps_beta_photo_running.py
@@ -430,7 +430,7 @@ def TEST_img_guide_drive(thd_distance_goal=10, thd_red_area=75):
                 ###-----PID制御により前進-----###
                 theta_array = [0]*5
                 PID.PID_run(target_azimuth, magx_off, magy_off, theta_array=theta_array, loop_num=20)
-                motor.deceleration(15. 15)
+                motor.deceleration(15, 15)
                 motor.motor_stop()
 
             ###-----撮像した画像の中にゴールが映っていない場合の処理-----###

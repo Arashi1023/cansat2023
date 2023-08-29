12a60912bf502dd9921b837558567f33a4d63be0
hayaocci <7319079@ed.tus.ac.jp>
2023-08-29 12:48:17 +0900

    ログのディレクトリ作成

--- cansat2023_main/e-ebetaver2.py
+++ cansat2023_main/e-ebetaver2.py
@@ -31,16 +31,27 @@ import beta_para_avoid as para_avoid
 import wgps_beta_photo_running as imgguide
 
 #variable for log
-log_phase=other.filename('/home/dendenmushi/cansat2023/sequence/log/phaselog/phaselog','txt')
-log_report=other.filename('/home/dendenmushi/cansat2023/sequence/log/reportlog/reportlog','txt')
-log_release=other.filename('/home/dendenmushi/cansat2023/sequence/log/releaselog/releaselog','txt')
-log_landing=other.filename('/home/dendenmushi/cansat2023/sequence/log/landinglog/landinglog','txt')
-log_melting=other.filename('/home/dendenmushi/cansat2023/sequence/log/meltinglog/meltinglog','txt')
-log_para=other.filename('/home/dendenmushi/cansat2023/sequence/log/para_avoid_log/para_avoid_log','txt')
-log_gpsrunning1=other.filename('/home/dendenmushi/cansat2023/sequence/log/gpsrunning1log/gpsrunning1log','txt')
-log_humandetect=other.filename('/home/dendenmushi/cansat2023/sequence/log/humandetectlog/humandetectlog','txt')
-log_gpsrunning2=other.filename('/home/dendenmushi/cansat2023/sequence/log/gpsrunning2log/gpsrunning2log','txt')
-log_photorunning =other.filename( '/home/dendenmushi/cansat2023/sequence/log/photorunninglog/photorunninglog','txt')
+# log_phase=other.filename('/home/dendenmushi/cansat2023/sequence/log/phaselog/phaselog','txt')
+# log_report=other.filename('/home/dendenmushi/cansat2023/sequence/log/reportlog/reportlog','txt')
+# log_release=other.filename('/home/dendenmushi/cansat2023/sequence/log/releaselog/releaselog','txt')
+# log_landing=other.filename('/home/dendenmushi/cansat2023/sequence/log/landinglog/landinglog','txt')
+# log_melting=other.filename('/home/dendenmushi/cansat2023/sequence/log/meltinglog/meltinglog','txt')
+# log_para=other.filename('/home/dendenmushi/cansat2023/sequence/log/para_avoid_log/para_avoid_log','txt')
+# log_gpsrunning1=other.filename('/home/dendenmushi/cansat2023/sequence/log/gpsrunning1log/gpsrunning1log','txt')
+# log_humandetect=other.filename('/home/dendenmushi/cansat2023/sequence/log/humandetectlog/humandetectlog','txt')
+# log_gpsrunning2=other.filename('/home/dendenmushi/cansat2023/sequence/log/gpsrunning2log/gpsrunning2log','txt')
+# log_photorunning =other.filename( '/home/dendenmushi/cansat2023/sequence/log/photorunninglog/photorunninglog','txt')
+
+log_phase=other.filename('../logs/ver2_logs/phaselog/phaselog','txt')
+log_report=other.filename('../logs/ver2_logs/reportlog/reportlog','txt')
+log_release=other.filename('../logs/ver2_logs/releaselog/releaselog','txt')
+log_landing=other.filename('../logs/ver2_logs/landinglog/landinglog','txt')
+log_melting=other.filename('../logs/ver2_logs/meltinglog/meltinglog','txt')
+log_para=other.filename('../logs/ver2_logs/para_avoid_log/para_avoid_log','txt')
+log_gpsrunning1=other.filename('../logs/ver2_logs/gpsrunning1log/gpsrunning1log','txt')
+log_humandetect=other.filename('../logs/ver2_logs/humandetectlog/humandetectlog','txt')
+log_gpsrunning2=other.filename('../logs/ver2_logs/gpsrunning2log/gpsrunning2log','txt')
+log_photorunning =other.filename( '../logs/ver2_logs/photorunninglog/photorunninglog','txt')
 
 def get_locations(lat_human, lon_human):
 #最後の位置情報をもとに周囲の4つの点の座標を求める

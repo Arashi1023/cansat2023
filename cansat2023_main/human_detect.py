a574cb3bd7227aee0b9beeab9999742f2b94ca5b
hayaocci <7319079@ed.tus.ac.jp>
2023-09-01 18:35:17 +0900

    a

--- cansat2023_main/human_detect.py
+++ cansat2023_main/human_detect.py
@@ -144,8 +144,8 @@ if __name__ == '__main__':
     stuck_check_array = deque([0]*6, maxlen=6)
     add_pwr = 0
     add_count = 0
-    magx_off = 0
-    magy_off = 0
+    # magx_off = 0
+    # magy_off = 0
 
     magx_off, magy_off = calibration.cal(30, -30, 30)
 

11d5b5b2e3c3e036e03cfcc8d61dcc633335c0e4
hayaocci <7319079@ed.tus.ac.jp>
2023-09-01 19:54:08 +0900

    さっきとおなじ修正

--- cansat2023_main/PID.py
+++ cansat2023_main/PID.py
@@ -652,7 +652,7 @@ if __name__ == "__main__":
 
     #-----オフセットの取得-----#
     #-----キャリブレーション-----#
-    print('Start Calibration')
+    # print('Start Calibration')
     # magx_off, magy_off = calibration.cal(30, -30, 40)　<-これがあるせいでおそらく2回キャリブレーションをしていた。
 
     #-----PID制御-----#

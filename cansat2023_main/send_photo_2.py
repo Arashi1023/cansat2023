530f592de790170ecb798bf8b4c24f4948d8fec5
kokom24 <everyoneis1213@gmail.com>
2023-09-02 18:09:10 +0900

    aaa

--- cansat2023_main/send_photo_2.py
+++ cansat2023_main/send_photo_2.py
@@ -435,12 +435,15 @@ if __name__ == '__main__':
     
     # 入力ファイルパスと出力ファイルパスを指定してリサイズ
     input_file = gray_image    # 入力ファイルのパスを適切に指定してください
-    photo_name = "/home/dendenmushi/cansat2023/imags/wireless_test/test.jpg"  # 出力ファイルのパスを適切に指定してください
     new_width = 60            # リサイズ後の幅を指定します
-    new_height = 80           # リサイズ後の高さを指定します
+    new_height = 80 
+    gray_image = cv2.resize(gray_image, (new_height, new_width))
+    cv2.imwrite(input_file, gray_image)
+    # photo_name = "/home/dendenmushi/cansat2023/imags/wireless_test/test.jpg"  # 出力ファイルのパスを適切に指定してください
+              # リサイズ後の高さを指定します
 
     # リサイズを実行
-    resize_image(input_file, photo_name, new_width, new_height)
+    # resize_image(input_file, photo_name, new_width, new_height)
     
     print("写真撮影完了")
 

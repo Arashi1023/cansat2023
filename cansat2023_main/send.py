004a66a0d8ea5814e72d8f891fcef334ea5e702d
kokom24 <everyoneis1213@gmail.com>
2023-09-04 20:09:23 +0900

    s

--- cansat2023_main/send.py
+++ cansat2023_main/send.py
@@ -17,7 +17,6 @@ def receive_data(port='/dev/ttyAMA0', baudrate=19200):
     IM920Serial = serial.Serial(port, baudrate)
     IM920Serial.flushInput()  # 入力バッファをクリア
     received_data = IM920Serial.readline().strip().decode()  # データを読み込み、改行文字を削除
-    print(receive_data)
     return received_data
 
 def send_reset(t_reset = 10):

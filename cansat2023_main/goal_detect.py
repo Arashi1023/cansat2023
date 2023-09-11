bf195441bb5686dde08c1140b453b123a6c9eea5
Shun Taguchi <taguchi@kimura-lab.net>
2023-09-11 17:38:51 +0900

    ゴール検知

--- cansat2023_main/goal_detect.py
+++ cansat2023_main/goal_detect.py
@@ -36,12 +36,12 @@ def detect_red(small_img):
     hsv_img = cv2.cvtColor(small_img, cv2.COLOR_BGR2HSV)
     
     # 赤色のHSVの値域1
-    red_min = np.array([0,105,20])
-    red_max = np.array([13,255,255])
+    red_min = np.array([0,30,30])
+    red_max = np.array([15,255,255])
     mask1 = cv2.inRange(hsv_img, red_min, red_max)
     
     # 赤色のHSVの値域2
-    red_min = np.array([160,105,20])
+    red_min = np.array([142,30,30])
     red_max = np.array([179,255,255])
     mask2 = cv2.inRange(hsv_img, red_min, red_max)
     

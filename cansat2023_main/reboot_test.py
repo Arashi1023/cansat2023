768a7e4de0af48f21b9917efe21836946954a9ab
Shun Taguchi <taguchi@kimura-lab.net>
2023-09-13 11:21:36 +0900

    a

--- cansat2023_main/reboot_test.py
+++ cansat2023_main/reboot_test.py
@@ -1,8 +1,24 @@
 import time
 
-try:
-    while True:
-        print("a")
-        time.sleep(1)
-except KeyboardInterrupt:
-    print("kill")
+# try:
+#     while True:
+#         print("a")
+#         time.sleep(1)
+# except KeyboardInterrupt:
+#     print("kill")
+
+import log
+
+reboot_log = log.Log(dir='log/reboot_log', filename='reboot_test', t_start=0, columns=['reboot_test'])
+
+reboot_checker = 0
+
+a = time.time()
+
+while : time.time() - a < 30:
+    reboot_log.save_log(reboot_checker)
+    reboot_checker += 1
+    time.sleep(1)
+
+
+
